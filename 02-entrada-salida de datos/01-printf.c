8ae399858a9e0134b8f518247a86b542b1f7242a
Marcelo Moglione <moglione@gmail.com>
2024-07-21 15:56:06 -0300

    Add files via upload

---
+++ 02-entrada-salida de datos/01-printf.c
@@ -0,0 +1,64 @@
+/*
+---------------------------------------------------------------------------------------------------------------------------------
+ Este ejemplo trata sobre como utilizar la funcion printf en lenguaje c.
+ - Printf() es una de las funciones de salida principales en la programacion en C. 
+ - La funcion envia una salida formatead a la pantalla.
+ - Debemos usar la declaracion #include <stdio.h> para incluir el archivo de encabezado stdio.h para poder usar printf() 
+ - Usamos el especificador de formato %d para imprimir tipos int. El %d se reemplaza por el valor de la variable.
+ - Para imprimir flotantes, utilizamos el especificador de formato %f.
+ - para imprimir caracteres, utilizamos el especificador de formato %c.
+ - Tambien se pueden realizar algunas operaciones aritmeticas dentro de la funcion.
+ - La funcion printf devuelve el numero de caracteres impresos o un valor negativo si se produce un error.
+-----------------------------------------------------------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>   // Se debe utilizar la declaracion "#include <stdio.h>"" 
+                     // si queremos utilizar printf().
+
+int main() {  // la funcion principal empieza aca
+  
+  // imprime la cadena completa usando comillas dobles ("").
+  printf("Hello World \n");  // \n se utiliza para generar un nuevo renglon (salto de linea)
+
+  // printf devuelve el numero de caracteres impresos
+  // por la tanto la variable X debera tener la cantidad de caracteres de "Hola Sanjo 8013"
+  int x = printf("Hola Sanjo 8013");
+  printf("\n %d \n", x);  // 15 es la salida (nota: se cuenta el caracter de espacio)
+
+  // se declaran e inicializan las variables
+  int a = 10;
+  char c = 'c';
+  float f = 12.3;
+  
+
+  // Para imprimir tipos int, utilizamos el especificador de formato %d. 
+  // El valorde la variable se sustituye por %d.
+  printf("Integer a = %d \n", a);
+
+  // Para imprimir el tipo char, se utiliza %c.
+  printf("Character c = %c \n", c);
+
+  // Para imprimir flotante, usamos el especificador de formato %f.
+  printf("Float f = %f \n", f);
+
+  // Puede realizar alguna operacion dentro de la funcion printf y se mostrara el resultado.
+  printf("a + f = %f \n", a+f);
+
+  return 0;
+}
+
+// aca termina la funcion principal (main)
+
+/*
+-------------------------------------------------- ------------------------------------------------------------------------------------
+Desafio: a dos  variables llamadas a y b asignele dos numeros y luego imprima el numero de caracteres 
+que tiene cada numero. Luego imprima la suma del numero de elementos presentes en a y b.
+ ejemplo:
+ - a = 12343212  
+ - b = 8877.88
+ - el numero de caracteres en a es 8 y en b es 7.
+ - la suma es 15.
+
+
+---------------------------------------------------------------------------------------------------- ----------------------------------
+*/
